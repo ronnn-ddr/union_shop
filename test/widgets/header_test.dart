d54aabf6cfee5174885cf167b001d36a233cdbc8
Ronnie Smith-Drake <ronnie.2smithdrake@icloud.com>
2025-11-16 19:56:48 +0000

    Created header tests

---
+++ test/widgets/header_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/widgets/header_widget.dart';
+
+void main() {
+  group('HeaderWidget Tests', () {
+  }
