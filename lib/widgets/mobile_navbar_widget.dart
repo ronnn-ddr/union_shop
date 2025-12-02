5dbb6da9f019d2102524579fc07063b8318793cc
Ronnie Smith-Drake <ronnie.2smithdrake@icloud.com>
2025-12-02 12:21:49 +0000

    Implement Navbar column layout and buttons

--- lib/widgets/mobile_navbar_widget.dart
+++ lib/widgets/mobile_navbar_widget.dart
@@ -5,6 +5,37 @@ class MobileNavbarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Shop', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+          ListTile(
+            leading: const Icon(Icons.print),
+            title: const Text('The Print Shack', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+          ListTile(
+            leading: const Icon(Icons.local_offer),
+            title: const Text('SALE!', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('About', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+          ListTile(
+            leading: const Icon(Icons.web),
+            title: const Text('UPSU.net', style: TextStyle(fontFamily: 'WorkSans')),
+          ),
+        ],
+      ),
+    );
   }
 }
