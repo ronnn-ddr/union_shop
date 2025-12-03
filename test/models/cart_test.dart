42b1780c79e66ecd7ff5d6f14c1601f28556e17c
Ronnie Smith-Drake <ronnie.2smithdrake@icloud.com>
2025-12-03 16:21:51 +0000

    Create cart test structure

---
+++ test/models/cart_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/models/cart.dart';
+
+void main() {
+  late Cart cart;
+
+  setUp(() {
+    // Create a fresh Cart instance before each test
+    cart = Cart();
+  });
+
+  group('Add Item Tests', () {
+    // Tests for adding items will go here
+  });
+
+  group('Update Quantity and Remove Tests', () {
+    // Tests for updating quantities and removal will go here
+  });
+
+  group('Clear Cart and Listener Notifications', () {
+    // Tests for clearing cart and listener notifications will go here
+  });
+}
