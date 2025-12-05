ea25aefd5a3fcac8d290299e74ca10986df3d983
Ronnie Smith-Drake <ronnie.2smithdrake@icloud.com>
2025-12-05 10:19:25 +0000

    Create collections test structure

---
+++ test/collection_page_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/collection_page.dart';
+import 'package:union_shop/widgets/header_widget.dart';
+import 'package:union_shop/widgets/footer_widget.dart';
+import 'package:union_shop/data/products.dart';
+import 'package:union_shop/models/product.dart';
+import 'package:union_shop/product_page.dart';
+
+void main() {
+  group('CollectionPage Rendering Tests', () {
+    // Basic rendering tests will go here
+  });
+
+  group('CollectionPage Product Display Tests', () {
+    // Product display tests will go here
+  });
+
+  group('CollectionPage Navigation Tests', () {
+    // Navigation tests will go here
+  });
+
+  group('CollectionPage Mobile Features Tests', () {
+    // Mobile drawer tests will go here
+  });
+
+  group('CollectionPage Edge Cases Tests', () {
+    // Empty collection tests will go here
+  });
+}
